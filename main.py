e32c0094f815c3b5e3bb9f5a5a951f03f04a7291
vishSP <hihi64741@gmail.com>
2023-04-25 02:16:51 +0400

    написал функции записи в дб

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+from classes import DBManager
 from utils import *
 from classes import *
 
@@ -8,12 +9,11 @@ def main():
     params = config()
 
     data = get_requests()
-    hh = HH(get_requests())
-
-    another_data = hh.get_ids()
 
     create_database('hh_work', params)
-    save_data_to_database(data, another_data, 'hh_work', params)
+    save_data_to_database(data, 'hh_work', params)
+    db = DBManager()
+    db.get_companies_and_vacancies_count()
 
 
 if __name__ == '__main__':
